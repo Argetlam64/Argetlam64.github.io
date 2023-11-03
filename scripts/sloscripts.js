9d41c8a58b0f14d19f7d38e5c114791242824eb2
Argetlam64 <majverhovnikmarovsek@gmail.com>
2023-11-03 20:40:47 +0100

    added scripts

---
+++ scripts/sloscripts.js
@@ -0,0 +1,61 @@
+const ok1 = "Hello!";
+const ok2 = "Hello!";
+const ok3 = "Hello!";
+const ok4 = "Hello!";
+const ok5 = "Hello!";
+const ok6 = "Hello!";
+const ok7 = "Hello!";
+const ok8 = "Hello!";
+const ok9 = "Hello!";
+const ok10 = "Hello!";
+const ak1 = "Hello!";
+const ak2 = "Hello!";
+const ak3 = "Hello!";
+const ak4 = "Hello!";
+const ak5 = "Hello!";
+const ak6 = "Hello!";
+const ak7 = "Hello!";
+const ak8 = "Hello!";
+const ak9 = "Hello!";
+const ak10 = "Hello!";
+const sk1 = "Hello!";
+const sk2 = "Hello!";
+const sk3 = "Hello!";
+const sk4 = "Hello!";
+const sk5 = "Hello!";
+const sk6 = "Hello!";
+const ls1 = "Hello!";
+const ls2 = "Hello!";
+const ls3 = "Hello!";
+const ls4 = "Hello!";
+const rk1 = "Hello!";
+const rk2 = "Hello!";
+const rk3 = "Hello!";
+const rk4 = "Hello!";
+const rk5 = "Hello!";
+const rk6 = "Hello!";
+const spk1 = "Hello!";
+const spk2 = "Hello!";
+const spk3 = "Hello!";
+const spk4 = "Hello!";
+const pb1 = "Hello!";
+const pb2 = "Hello!";
+const pb3 = "Hello!";
+const er1 = "Hello!";
+const er2 = "Hello!";
+const er3 = "Hello!";
+const sr1 = "Hello!";
+const sr2 = "Hello!";
+const sr3 = "Hello!";
+const sr4 = "Hello!";
+
+
+
+
+showText = (text,answer) =>{
+    if (document.getElementById(answer).innerHTML === ""){
+        document.getElementById(answer).innerHTML = text;
+    }
+    else{
+        document.getElementById(answer).innerHTML = ""}
+}
