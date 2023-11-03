c8a23ac500bc629816508246211ea028a1100e89
Argetlam64 <majverhovnikmarovsek@gmail.com>
2023-11-03 21:35:45 +0100

    script comments for easier filling, variables instead of names

--- scripts/sloscripts.js
+++ scripts/sloscripts.js
@@ -1,53 +1,130 @@
+//Orientalske književnosti
+//Kaj označujemo z izrazom orientalske književnosti?
 const ok1 = "Hello!";
+//Orientalske književnosti: čas in prostor, književne zvrsti in vrste, vpliv verstev.
 const ok2 = "Hello!";
+//Naštejte najpomembnejše "svete knjige".
 const ok3 = "Hello!";
+//Kje in kdaj se je razvila prva pisava? Začetki črkovne pisave.
 const ok4 = "Hello!";
+//V obliki česa je ohranjena hebrejska književnost?
 const ok5 = "Hello!";
+//Kulture različnosti branja Biblije. Razložite.
 const ok6 = "Hello!";
+//Biblija: nastanek, jezik, zgradba, kanonska beseda, apokrifi.
 const ok7 = "Hello!";
+//Kulturni in literalni pomen Biblije, prvi prevodi(septuganita, vulgata).
 const ok8 = "Hello!";
+//Kaj je prilika? Prilika o izgubljenem sinu, ideja prilike.
 const ok9 = "Hello!";
+//Razložite naslednje biblične framzeme: biti v adamovm kostumu, sad spoznanja, prah si in v prah se povrneš, v potu svojega obraza, adamovo jabolko
 const ok10 = "Hello!";
+
+//Antična književnost
+//Kaj označuje pojem antika? Kakšen je prvotni pomen besede? Čas in prostor.
 const ak1 = "Hello!";
+//Obdobja v starogrški književnosti, zvrsti in vrste, najpomembnejši antični pisci.
 const ak2 = "Hello!";
+//Straogrško pripovedništvo: ep, vrste epov, mit/bajka.
 const ak3 = "Hello!";
+//Homer, homersko vprašanje. Od kje je vzel snov za Ilijado in Odisejo?
 const ak4 = "Hello!";
+//Ilijada: mit o Trojanski vojni, dogajane, poseganje bogov, glavni motiv, Ahil in Hektor, zgradba.
 const ak5 = "Hello!";
+//Odiseja: Odisejeve pustolovščine, zgodba o Polifemu, zvijača, okvirna in vložena pripoved, analitična pripovedna tehnika.
 const ak6 = "Hello!";
+//Starogrška dramatika: nastanek, uprizarjanje, zgradba gledališča, vloga zbora.
 const ak7 = "Hello!";
+//Značilnosti klasične tragedije; razložite pojma tragično in katarza.
 const ak8 = "Hello!";
+//Mit o Tebanski kraljevi hiši.
 const ak9 = "Hello!";
+//Sofokles, Kralj Ojdip: dogajanje, zgradba tragedije (pravilo trojne enakosti,dramski trikotnik), tragični junak, Ojdipov kompleks.
 const ak10 = "Hello!";
+
+//Srednjeveška književnost
+//Čas in prostor; obdobja srednjega veka; tematika (posvetna, cerkvena)
 const sk1 = "Hello!";
+//Književne zvrsti in vrste: trubadursko pesništvo, vagantsko pesništvo, sladki novi stil, meščansko pesništvo; epika; dramatika.
 const sk2 = "Hello!";
+//Srednjeveška književnost na Slovenskem (zgodovinske okoliščine).
 const sk3 = "Hello!";
+//Slovenski rokopisi.
 const sk4 = "Hello!";
+//Brižinski spomeniki: nastanek, pisava, jezik. Kako so sestavljeni?
 const sk5 = "Hello!";
+//II. Brižinski spomenik: Pridiga o grehu in pokori, povzetek besedila. Komu je pridiga namenjena?
 const sk6 = "Hello!";
+
+//Ljudsko slovstvo
+//Kaj je mit? Slovanska božanstva in bajeslovna bitja, ljudski običaji.
 const ls1 = "Hello!";
+//Slovensko ljudsko slovstvo: čas nastanka, značilnosti, anonimnost, variante, književne zvrsti.
 const ls2 = "Hello!";
+//Ljudsko presništvo: vrste ljudskih pesmi, primeri.
 const ls3 = "Hello!";
+//Zeleni Jurij: obredna pesem, jurjevanje, darovanje, sporočilo, jezik in slog.
 const ls4 = "Hello!";
+
+//Renesančna književnost
+//Časovna in prostorska umestitev; razlaga pojmov renesansa in humanizem; ideali renesančnega človeka.
 const rk1 = "Hello!";
+//Renesančna književnost: književne zvrsti in vrste, predstavniki.
 const rk2 = "Hello!";
+//Začetki in razvoj renesančnega gledališča; znani dramatiki.
 const rk3 = "Hello!";
+//W. Shakespeare, najpomembnejše drame; tragedija Romeo in Julija: povzetek vsebine, snov, glavni motiv, dogajalni čas in prostor, ideja.
 const rk4 = "Hello!";
+//Cervantes, Don Kihot: zgodba; razlaga pojmov: viteški roman, parodija, satira, iluzija, tragikomičnost; Don Kihot in Sančo Pansa.
 const rk5 = "Hello!";
+//Boccacio, Dekameron: značilnosti novele, Dekameron (okvirna zgodba, zgodbe po vsebini, ideja), Novela o sokolu.
 const rk6 = "Hello!";
+
+//Slovenska protestantska književnost
+//Razlaga pojmov reformacija in protestantizem; čas in prostor, zahteve.
 const spk1 = "Hello!";
+//Predstavniki slovenskega ptorestantizma.
 const spk2 = "Hello!";
+//Prvi slovenski knjigi, nastanek knjižnega jezika, pisava.
 const spk3 = "Hello!";
+//Trubar, Proti zidavi cerkva: razlaga pridige, izvirnost besedila, protestantske ideje v pridigi, jezikovne značilnosti.
 const spk4 = "Hello!";
+
+//Protireformacina in barok
+//Razlaga pojmov protireformacija in barok, čas in prostor, značilnosti.
 const pb1 = "Hello!";
+//Predstavniki sloveske baročne književnosti.
 const pb2 = "Hello!";
+//Svetokriški, Na noviga lejta dan: baročna pridiga, polliteralno besedilo, zgled ali eksempel; jezik in baročni slog.
 const pb3 = "Hello!";
+
+//Evropska romantika
+//Evropska romantika, razlaga pojma, čas in prostor, značilnosti; predstavniki evropske romantike.
 const er1 = "Hello!";
+//Romantični junak, svetobolje, razkol med ideali in stvarnostjo.
 const er2 = "Hello!";
+//Puškin, Jevgenij Onjegin: roman v verzih, zgradba, zgodba, oznaka oseb, pismo kot del književnega besedila, romantična ljubezen, odvečni človek.
 const er3 = "Hello!";
+
+//Slovenska romantika
+//Romantika na Slovenskem, čas, predstavniki, pomen slovenske romantike; Prešernov pesniški razvoj.
 const sr1 = "Hello!";
+//Prešeren, Krst pri savici: zgodovinsko ozadje, zgradba, epski-lirska pesnitev; Črtomir v Uvodu in Črtomir v Krstu; Črtomirove odločitve.
 const sr2 = "Hello!";
+//Prešeren, Sonetni venec: sonet, zunanje zgradba (Magistrale, akostrih), notranja zgradba (prepletanje tem), ideja, interpretacija sonetov.
 const sr3 = "Hello!";
+//Prešeren, Zdravljica: napitnica/zdravnica, zgradba pesmi, likovna pesem, nacionalna in politična ideja pesmi, cenzura, slovenska himna.
 const sr4 = "Hello!";
+
+//Realizem
+//
+const rl1 = "Hello!";
+//
+const rl2 = "Hello!";
+//
+const rl3 = "Hello!";
+//
+const rl4 = "Hello!";
 
 
 
